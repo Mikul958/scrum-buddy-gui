b108e8a16a6e01138a5fdff31356e8261de35324
Mikul958 <michaelpikula958@gmail.com>
2023-12-05 14:16:30 -0500

    Added setOnCloseRequest method

--- gui/src/main/java/scrumsystem/App.java
+++ gui/src/main/java/scrumsystem/App.java
@@ -1,12 +1,17 @@
 package scrumsystem;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
+
+import model.*;
 
 /**
  * JavaFX App
@@ -20,6 +25,15 @@ public class App extends Application {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent w) {
+                ScrumSystem.getInstance().saveData();
+                ScrumSystem.getInstance().logout();
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 
     static void setRoot(String fxml) throws IOException {
