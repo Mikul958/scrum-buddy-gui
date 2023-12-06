c0ce2cd6812dd1efdb8da87b6af2b36a4c02b4da
Mikul958 <michaelpikula958@gmail.com>
2023-12-06 11:27:21 -0500

    Added event handlers to labels

--- gui/src/main/java/scrumsystem/DashboardController.java
+++ gui/src/main/java/scrumsystem/DashboardController.java
@@ -1,18 +1,29 @@
 package scrumsystem;
 
+import java.util.ArrayList;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.fxml.Initializable;
 import model.*;
 
-public class DashboardController implements Initializable {
+public class DashboardController implements Initializable
+{   
+    @FXML
+    VBox projectVBox;
+    
+    ScrumSystem system = ScrumSystem.getInstance();
+    ArrayList<Project> currentProjects = system.getCurrentAccountProjects();
 
     @FXML
     private Label lbl_username;
@@ -21,6 +32,22 @@ public class DashboardController implements Initializable {
     public void initialize(URL arg0, ResourceBundle arg1) {
         ScrumSystem system = ScrumSystem.getInstance();
         lbl_username.setText("Dashboard");
+        for (int i=0; i<currentProjects.size(); i++)
+        {
+            HBox project = new HBox();
+            projectVBox.getChildren().add(project);
+
+            Label label = new Label();
+            label.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    System.out.println(label.getText() + " is clicked");
+                }
+                
+            });
+            label.setText(currentProjects.get(i).getTitle());
+            project.getChildren().add(label);
+        }
     }
     
     @FXML
