2f6ac51112bada4d0d7ddfd88c37b416b0439843
mwwedeking <111778304+mwwedeking@users.noreply.github.com>
2023-12-06 15:05:31 -0500

    Added: Back to dashboard button from Projects page

--- gui/src/main/java/scrumsystem/ProjectController.java
+++ gui/src/main/java/scrumsystem/ProjectController.java
@@ -1,8 +1,10 @@
 package scrumsystem;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -17,6 +19,11 @@ public class ProjectController implements Initializable
     public void initialize(URL arg0, ResourceBundle arg1) {
         ScrumSystem project = ScrumSystem.getInstance();
         System.out.println("in here");
-        totalTasks.setText("" + project.getCurrentProject().getTotalTasks());
+        totalTasks.setText("Number of tasks: " + project.getCurrentProject().getTotalTasks());
+    }
+
+    @FXML
+    void returnToDashboard(ActionEvent event) throws IOException {
+        App.setRoot("dashboard");
     }
 }
