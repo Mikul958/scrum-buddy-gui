c2ae4e3bc2c951cfd8a37a72d794d1cacd83e265
Mikul958 <michaelpikula958@gmail.com>
2023-12-06 10:26:14 -0500

    Made buttons on Signup functional

--- gui/src/main/java/scrumsystem/SignUpController.java
+++ gui/src/main/java/scrumsystem/SignUpController.java
@@ -1,6 +1,8 @@
 package scrumsystem;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -14,4 +16,37 @@ public class SignUpController
 
     @FXML
     private PasswordField textPassword, textPasswordConfirm;
+
+    @FXML
+    void backToLogin() throws IOException
+    {
+        App.setRoot("primary");
+    }
+
+    @FXML
+    void signUp() throws IOException
+    {
+        String password = textPassword.getText();
+        String passwordConfirm = textPasswordConfirm.getText();
+        if (!password.equals(passwordConfirm))
+        {
+            // TODO make text box with confirm password error.
+            return;
+        }
+        String username = textUsername.getText();
+        String email = textEmail.getText();
+        String firstName = textFirstName.getText();
+        String lastName = textLastName.getText();
+        boolean success = ScrumSystem.getInstance().createAccount(username, password, email, firstName, lastName);
+
+        if (success)
+        {
+            ScrumSystem.getInstance().login(username, password);
+            App.setRoot("dashboard");
+        }
+        else
+        {
+            // TODO make text box with general error
+        }
+    }
 }
