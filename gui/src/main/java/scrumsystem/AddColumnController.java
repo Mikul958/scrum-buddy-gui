b86e94ba4857cd6a64eb87aca2eee7a4693d78d4
mwwedeking <111778304+mwwedeking@users.noreply.github.com>
2023-12-07 18:41:25 -0500

    Added create column screen, and cancel button

--- gui/src/main/java/scrumsystem/AddColumnController.java
+++ gui/src/main/java/scrumsystem/AddColumnController.java
@@ -1,6 +1,14 @@
 package scrumsystem;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class AddColumnController
 {
-    
+    @FXML
+    void cancelAddColumn(ActionEvent event) throws IOException{
+        App.setRoot("project");
+    }
 }
