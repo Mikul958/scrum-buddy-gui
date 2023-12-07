d578b05207f630a1026c73a51e0f50801b973e85
mwwedeking <111778304+mwwedeking@users.noreply.github.com>
2023-12-07 18:55:04 -0500

    Added text field to add column screen

--- gui/src/main/java/scrumsystem/AddColumnController.java
+++ gui/src/main/java/scrumsystem/AddColumnController.java
@@ -4,9 +4,14 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class AddColumnController
 {
+
+    @FXML
+    private Label labelProjectName;
+
     @FXML
     void cancelAddColumn(ActionEvent event) throws IOException{
         App.setRoot("project");
