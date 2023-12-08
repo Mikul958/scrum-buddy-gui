a24e0fdd69e2123f97a7f670626f5e44477519a8
mwwedeking <111778304+mwwedeking@users.noreply.github.com>
2023-12-07 19:09:57 -0500

    Added label to add column screen

--- gui/src/main/java/scrumsystem/AddColumnController.java
+++ gui/src/main/java/scrumsystem/AddColumnController.java
@@ -1,16 +1,29 @@
 package scrumsystem;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import model.ScrumSystem;
 
-public class AddColumnController
+public class AddColumnController implements Initializable
 {
 
     @FXML
     private Label labelProjectName;
+
+    ScrumSystem system = ScrumSystem.getInstance();
+
+    @FXML
+    public void initialize(URL arg0, ResourceBundle arg1){
+
+        labelProjectName.setText("Current Project: "+system.getCurrentProject().getTitle());
+
+    }
 
     @FXML
     void cancelAddColumn(ActionEvent event) throws IOException{
